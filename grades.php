22ef0cf89686b0be2725600195c1a7436b5089ea
dimrio323 <62644864+dimrio323@users.noreply.github.com>
2020-11-15 19:58:31 +0500

    изменения

--- grades.php
+++ grades.php
@@ -45,7 +45,7 @@ if(isset($_COOKIE['SSSIDH'])){
                 <li> <a href="student.php">Главная</a></li>
                 <li> <a href="grades.php"> Смотреть свои оценки </a> </li>
                 <li> <a href="communication.php"> Общение с преподавателями </a> </li>
-                <li> <a href="#"> Объявления школы </a> </li>
+                <li> <a href="announce.php"> Объявления школы </a> </li>
                 <li> <a href="php/functions/logout.php">Выйти из аккаунта</a>  </li>
                 <br>
                 <li> <span id='current-lesson'> </span> </li>
