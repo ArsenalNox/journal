0404087592ab6e9698a802fae3972b7cbe9ea5cc
ArsenalNox <nohtimore@gmail.com>
2020-11-15 07:59:43 +0500

    Тайтл + ориентация в манифесте

--- index.php
+++ index.php
@@ -46,17 +46,20 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <head>
   <meta charset="utf-8">
   <meta name="theme-color" content="#FFFFFF"/>
-  <link rel="manifest" href="manifest.webmanifest">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="theme-color" content="#FFFFFF"/>
+  <meta
+    name="description"
+    content="Прогрессивное веб приложение электронного журнала оренбургской области">
+  <link rel="manifest" href="manifest.webmanifest">
   <link rel="stylesheet" href="css/main.css">
   <link rel="apple-touch-icon" sizes="192x192" href="img/192.png">
   <link rel="apple-touch-icon" sizes="48x48" href="img/48.png">
   <link rel="apple-touch-icon" sizes="57x57" href="img/57.png">
-  <!-- <link rel="apple-touch-icon" sizes="114x114" href="img/114.png"> -->
   <link rel="apple-touch-icon" sizes="512x512" href="img/512.png">
   <link rel="apple-touch-icon" sizes="1024x1024" href="img/1024.png">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="img/48.png" />
+  <!-- <link rel="apple-touch-icon" sizes="114x114" href="img/114.png"> -->
   <title>Вход в ЭлЖур</title>
 </head>
 
